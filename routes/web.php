749a1780ac0c3a68d35d068f5575e9e396d50598
dulnith_thathsara <115772147+dulniththathsara@users.noreply.github.com>
2025-08-13 13:20:01 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\AddTechnicianController;
+use App\Http\Controllers\TechnicianDBController;
+use App\Http\Controllers\AdminDBController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\RequestFormController;
 use App\Http\Controllers\LandingPageController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
 Route::get('/', [LandingPageController::class, 'index']);
-Route::get('/request-form', [RequestFormController::class, 'show'])->name('request.form');
+Route::get('/request-form', [CustomerController::class, 'index'])->name('request.form');
 Route::get('/customer-dashboard', [CustomerController::class, 'index'])->name('customer.dashboard');
+Route::get('/admin-db', [AdminDBController::class, 'index'])->name('admin.db');
+Route::get('/add-technician', [AddTechnicianController::class, 'index'])->name('add.technician');
+Route::get('/technician-db', [TechnicianDBController::class, 'index'])
+    ->name('technician.db');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
